8e7cbe4cc1f80120ad4839332aaf34ede97172ba
Stephane_tcha <stephanetcha@gmail.com>
2023-05-16 17:36:41 +0200

    first commit

---
+++ src/main/java/com/projet/gestionLocation/GestionLocationApplication.java
@@ -0,0 +1,13 @@
+package com.projet.gestionLocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionLocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projet/gestionLocation/GestionLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.gestionLocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
